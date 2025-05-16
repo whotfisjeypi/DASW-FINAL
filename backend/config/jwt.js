ce5d865075cf2d0746308a2f5723d834898033eb
Baby_Reyes <luis.delosreyes@iteso.mx>
2025-05-16 01:04:06 -0600

    Borrar archivos/carpetas innecesarios

--- backend/config/jwt.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    secret: process.env.JWT_SECRET || 'cambiame_por_una_clave_segura',
-    expiresIn: '7d',
-};
--- backend/middleware/role.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(roles = []) {
-    if (typeof roles === 'string') roles = [roles];
-    return (req, res, next) => {
-        if (!roles.includes(req.user.role))
-            return res.status(403).json({ msg: 'Acceso denegado' });
-        next();
-    };
-};
